13bd9b78ffa59b291d4cfc4e7a64bb94bd0719f7
dlwnsqo <uyiuy97@naver.com>
2021-07-13 22:11:45 +0900

    Implement Customer Add Moudle & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import {withStyles} from '@material-ui/core/styles'
+import {withStyles} from '@material-ui/core/styles';
+import CustomerAdd from './components/CustomerAdd';
 
 const styles = theme => ({
   root:{
@@ -24,10 +25,24 @@ const styles = theme => ({
 
 class App extends Component{
 
-  state={
-    customers: ""
+  constructor(props){
+    super(props)
+      this.state = {
+        customers: '',
+        completed: 0
+      }
   }
 
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+  
   componentDidMount(){
     this.callApi()
     .then(res => this.setState({customers: res}))
@@ -43,37 +58,40 @@ class App extends Component{
   render(){
     const {classes}=this.props;
     return(
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-            </TableRow>
-          </TableHead>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+              </TableRow>
+            </TableHead>
 
-          <TableBody>
-          {
-            this.state.customers ? 
-            this.state.customers.map(c => {
-              return (
-                <Customer
-                  key = {c.id}
-                  id= {c.id}
-                  image={c.image}
-                  name = {c.name}
-                  birthday = {c.birthday}
-                  gender = {c.gender}
-               />
-              );
-           }) : ""
-          }
-          </TableBody>
-        </Table>
-      </Paper>
+            <TableBody>
+            {
+              this.state.customers ? 
+              this.state.customers.map(c => {
+                return (
+                  <Customer
+                    key = {c.id}
+                    id= {c.id}
+                    image={c.image}
+                    name = {c.name}
+                    birthday = {c.birthday}
+                    gender = {c.gender}
+                />
+                );
+            }) : ""
+            }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh = {this.stateRefresh}/>
+      </div>
     )
   }
 }
